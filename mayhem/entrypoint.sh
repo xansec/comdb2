a9e5fd6118adf0952cba318d9f38cc00267fb5d0
Alexander Brewer <abrewer@forallsecure.com>
2023-05-11 14:58:06 -0400

    Trying network stuff

--- mayhem/entrypoint.sh
+++ mayhem/entrypoint.sh
@@ -5,6 +5,6 @@ export PATH=$PATH:/comdb2/bb/bin
 DB_NAME=fuzzdb
 
 pmux -n
-comdb2 $DB_NAME &
-sleep 2
-exec cdb2sql $DB_NAME -f /input
+#comdb2 $DB_NAME &
+#sleep 2
+#exec cdb2sql $DB_NAME -f /input
